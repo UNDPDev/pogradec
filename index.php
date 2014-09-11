84548246f6863d7489dcb733831a98d296b4fd75
pnud <pnud@users.noreply.github.com>
2014-09-11 23:20:44 +0200

    Update index.php
    Added DB settings

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 echo "Ckemi cuna!";
 
 echo "sduket keq, po ky Denisi nga humbi thuaj??";
+
+/*
+TE DHENAT PER DB JANE:
+SERVER: localhost
+DB: dyqaniim_pogradec
+USER: dyqaniim_pg
+PASS: Pogradec2014
+*/
 ?>
